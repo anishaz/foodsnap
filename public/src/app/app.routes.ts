8d57f6cf46e9da685cbdafaa5af569ee66448ed7
AnishaZ <anishagzacharia@gmail.com>
2017-05-29 22:42:53 -0700

    Component to show in root switched

--- public/src/app/app.routes.ts
+++ public/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ImageGridComponent } from './image-grid/image-grid.component';
 
 const APP_ROUTES: Routes = [
-    { path: '', component: NavigationComponent },
+    { path: '', component: ImageGridComponent },
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegistrationComponent}
 ];
