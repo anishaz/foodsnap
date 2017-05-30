04dcb06fa41d0a418d6aaa3158455855ad0b4a83
AnishaZ <anishagzacharia@gmail.com>
2017-05-29 21:32:44 -0700

    Login method in controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,5 @@
 let controller = require('./../controllers/controller');
 
 module.exports = app => {
-
+  app.post('/api/login', controller.login);
 }
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -4,5 +4,16 @@ let User = mongoose.model('User');
 let Image = mongoose.model('Image');
 
 module.exports = {
+  login: (req,res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+      if(err){
+        console.log(err);
+        return res.sendStatus(500);
+      }else {
+        req.session.user = user;
+        return res.json(user);
+      }
+    })
+  },
 
 }
