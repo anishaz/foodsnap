b3826446faa06c430d83f3ec383863f4ac18d3d1
AnishaZ <anishagzacharia@gmail.com>
2017-06-01 14:49:20 -0700

    Modified validations

--- server/models/user.js
+++ server/models/user.js
@@ -5,18 +5,19 @@ let UserSchema = new mongoose.Schema({
   first_name: {
     type:String,
     required:[true,"First name is required."],
-    min: [3, "Minimum length is 3 characters."],
-    max: [20, "Maximum length is 20 characters."]
+    minlength: [2, "Minimum length is 2 characters."],
+    maxlength: [20, "Maximum length is 20 characters."]
   },
   last_name: {
     type:String,
     required:[true,"Last name is required."],
-    min: [3, "Minimum length is 3 characters."],
-    max: [20, "Maximum length is 20 characters."]
+    minlength: [2, "Minimum length is 2 characters."],
+    maxlength: [20, "Maximum length is 20 characters."]
   },
   email: {
     type:String,
     required:[true,"An email address is required."],
+    unique: [true, “This email has already been registered.“],
     lowercase: true,
     validate: function(email) {
       return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
