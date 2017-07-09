4266ef2f71296abd8a90063b3f3ff9bdc193e5a2
AnishaZ <anishagzacharia@gmail.com>
2017-07-09 14:33:42 -0700

    Layout adjustments for like button

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { LoginService } from './login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  user: any;
+
+  constructor(private _loginService: LoginService, private _router: Router) {}
+
+  ngOnInit () {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this._loginService.getCurrent()
+    .then( (user) => {
+      this.user = user;
+      console.log("logged in")
+    })
+      .catch( (err) => console.log(err))
+  }
+
 }
